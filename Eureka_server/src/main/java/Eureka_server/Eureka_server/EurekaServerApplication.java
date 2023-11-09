7bf4a82705fb828b46b993160b5431740bb0be98
shyammatta <mattashyam8@gmail.com>
2023-11-09 15:33:53 +0530

    shyam

---
+++ Eureka_server/src/main/java/Eureka_server/Eureka_server/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package Eureka_server.Eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+		System.out.println("eureka server is running");
+	}
+
+}
